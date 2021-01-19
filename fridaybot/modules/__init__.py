cce74c94a741b4313790c18b065d378e096a3d77
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2021-01-19 10:50:53 +0530

    Update __init__.py

--- fridaybot/modules/__init__.py
+++ fridaybot/modules/__init__.py
@@ -55,7 +55,7 @@ else:
     dbstats = "Not Fine ❌"
 
 inlinestats = (
-    f"✘ SHOWING VirtualUserbot STATS ✘\n"
+    f"✘ SHOWING Bruce Wayne's STATS ✘\n"
     f"VERSION = {currentversion} \n"
     f"DATABASE = {dbstats} \n"
     f"SUDO = {amiusingsudo} \n"
