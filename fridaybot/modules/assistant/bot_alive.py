871a163ce0d50288b85e498f77e2aaaa7d421856
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2021-01-19 10:18:29 +0530

    Update bot_alive.py

--- fridaybot/modules/assistant/bot_alive.py
+++ fridaybot/modules/assistant/bot_alive.py
@@ -16,19 +16,19 @@ from fridaybot import ALIVE_NAME
 from fridaybot.modules import currentversion
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
-PM_IMG = "https://telegra.ph/file/0e7a45ed44e17ce68d8cd.png"
-pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `master`\n"
-pm_caption += f"➥ **Version** : `{currentversion}`\n"
-pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [InukaASiTH](GitHub.com/Inukaasith)\n"
-pm_caption += "[Assistant By Inukaasith](https://t.me/Inukaasith)"
+PM_IMG = "https://telegra.ph/file/9de90ce6fb5093f92db7c.jpg"
+pm_caption = "➻ **ASSISTANT IS:** `ONLINE`\n\n"
+pm_caption += "➻ **SYSTEMS STATS**\n"
+pm_caption += "➻ **Telethon Version:** `1.15.0` \n"
+pm_caption += "➻ **Python:** `3.7.4` \n"
+pm_caption += "➻ **Database Status:**  `Functional`\n"
+pm_caption += "➻ **Current Branch** : `master`\n"
+pm_caption += f"➻ **Version** : `{currentversion}`\n"
+pm_caption += f"➻ **My Boss** : {DEFAULTUSER} \n"
+pm_caption += "➻ **Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += "➻ **License** : [GNU General Public License v3.0](github.com/StarkGang/FridayUserbot/blob/master/LICENSE)\n"
+pm_caption += "➻ **Copyright** : By [Bruce Wayne](https://t.me/BruceSL)\n"
+pm_caption += "[Assistant By Bruce Wayne](https://t.me/BruceSL)"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
