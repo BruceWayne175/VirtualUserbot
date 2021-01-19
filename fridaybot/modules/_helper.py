2c7cc237f20d53f1773be3316ba8352adaac3d9d
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2021-01-19 10:52:01 +0530

    Update _helper.py

--- fridaybot/modules/_helper.py
+++ fridaybot/modules/_helper.py
@@ -1,6 +1,6 @@
 from fridaybot import ALIVE_NAME, CMD_LIST, lang
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "VirtualUserbot"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Bruce Wayne"
 
 
 @command(pattern="^.help ?(.*)")
@@ -31,7 +31,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""VirtualUserbot Modules Are Listed Here !\n
+            help_string = f"""Bruce Wayne's Modules Are Listed Here !\n
 For More Help or Support contact {DEFAULTUSER}\nCurrently Used Language - {lang}"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
